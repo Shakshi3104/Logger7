e21ec766d6ec8caebdd70548e785dd2c6dd43134
Satoshi Kobayashi <pikanchisato@yahoo.co.jp>
2020-08-02 10:13:31 +0900

    Initial Commit

---
+++ Logger6 WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  Logger6 WatchKit Extension
+//
+//  Created by MacBook Pro on 2020/08/02.
+//  Copyright © 2020 MacBook Pro. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
---
+++ Logger6 WatchKit Extension/NotificationView.swift
@@ -0,0 +1,21 @@
+//
+//  NotificationView.swift
+//  Logger6 WatchKit Extension
+//
+//  Created by MacBook Pro on 2020/08/02.
+//  Copyright © 2020 MacBook Pro. All rights reserved.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
