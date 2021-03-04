5de22738df1ce7f83b2bb37ace6728f4c9f1303d
Shakshi3104 <watchosastrom3104@gmail.com>
2021-03-04 13:37:36 +0900

    Update directory structure

---
+++ Logger7/View/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  LoggerWatchPods
+//
+//  Created by Satoshi on 2020/10/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LogView()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
